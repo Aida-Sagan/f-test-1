1c72490448c8f1be07b5755029c6d9967a9427e7
Vitaly L <111348000+vetrof@users.noreply.github.com>
2024-09-06 16:55:15 +0500

    init

---
+++ project/catalog/views.py
@@ -0,0 +1,26 @@
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST
+from django.views.generic import ListView
+from catalog.models import Product
+
+
+class ProductsListView(ListView):
+    model = Product
+    template_name = 'catalog/products_list.html'
+    context_object_name = 'products'
+    queryset = Product.objects.filter(is_active=True)
+
+
+@csrf_exempt
+def delete_product(request, pk):
+    if request.method == 'POST':
+        if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+            product = get_object_or_404(Product, pk=pk)
+            product.is_active = False
+            product.save()
+            return JsonResponse({'success': True})
+        else:
+            return JsonResponse({'success': False}, status=400)
+    return JsonResponse({'success': False}, status=400)
